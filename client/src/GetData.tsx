c8e51d52e0fed1780907e5f0e312989a60098b6b
Gardenii <77850103+jwc406@users.noreply.github.com>
2022-07-08 10:16:15 +0900

    화면띄우기 성공 ㅠㅠ

--- client/src/GetData.tsx
+++ client/src/GetData.tsx
@@ -1,66 +1,28 @@
 import axios from "axios";
 import useSWR from "swr";
+import { job } from "./type/job";
 
-function container() {
-  async function getUrl(id: number) {
-    // eslint-disable-next-line react-hooks/rules-of-hooks
-    const { data } = useSWR(`http://localhost:3001/jw-users/${id}`, () =>
-      fetcher(`http://localhost:3001/jw-users/${id}`)
-    );
-    const res = data?.data;
-    return res;
-  }
-
-  const fetcher = (url: string) => axios.get(url);
-
-  const promise = getUrl(1);
-
-  async function getData() {
-    const a = await promise;
-    // console.log(a);
-    return a;
-  }
-
-  console.log(getData());
-
-  return (
-    <>
-      <h2>😙</h2>
-      <p>결과는 콘솔창에 ...</p>
-    </>
+function Container() {
+  const { data: abc } = useSWR(`/jw-users`, () =>
+    fetcher(`http://localhost:3001/jw-users/`)
   );
+  const fetcher = (url: string) => axios.get(url);
+  console.log(abc); //axios 응답 -> json 형태
+
+  // const resultData = getData();
+  // console.log("data:", data?.data[0].name);
+  const getName = abc?.data[0].name;
+  // console.log("된다!", abc);
+
+  return abc?.data.map((value: job, index: any) => {
+    return (
+      <>
+        <div>{value.id}</div>
+        <div>{value.name}</div>
+        <div>{value.job}</div>
+      </>
+    );
+  });
 }
 
-export default container;
-
-// function getData() {
-//   promise.then((gData) => {
-//     console.log(gData);
-//     return gData;
-//   });
-// }
-
-//   getData(2);
-//   async function getData(id: number) {
-//     try {
-//       const data = await fetch(`http://localhost:3001/jw-users/${id}`, {
-//         headers: {
-//           Accept: "application / json",
-//         },
-//       })
-//         .then((res) => res.json())
-//         .then((res) => {
-//           console.log(res);
-//         });
-//       return data;
-//     } catch (error) {
-//       console.log(error);
-//     }
-//   }
-//   return (
-//     <>
-//       <h2>😙</h2>
-//       <p>결과는 콘솔창에 ...</p>
-//     </>
-//   );
-// }
+export default Container;
